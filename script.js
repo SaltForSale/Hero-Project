4dc152291b3fb36edd44428f2e221c6b1ef70ac8
Salt <89614490+SaltForSale@users.noreply.github.com>
2023-02-21 10:34:26 -0500

    Commit 2

---
+++ script.js
@@ -0,0 +1,111 @@
+document.getElementById("avengers").style.display = "none";
+document.getElementById("cap").style.display = "none";
+document.getElementById("f4").style.display = "none";
+document.getElementById("galactus").style.display = "none";
+document.getElementById("hulk").style.display = "none";
+document.getElementById("iron").style.display = "none";
+document.getElementById("panther").style.display = "none";
+document.getElementById("surfer").style.display = "none";
+document.getElementById("thor").style.display = "none";
+document.getElementById("vision").style.display = "none";
+document.getElementById("xmen").style.display = "none";
+document.getElementById("antTab").className =
+	"accoridon highlight";
+function showAnt() {
+	hidePanels();
+	document.getElementById("ant").style.display = "block";
+	document.getElementById("antTab").className =
+	"accoridon highlight";
+}
+function showAvengers() {
+	hidePanels();
+	document.getElementById("avengers").style.display = "block";
+	document.getElementById("avengersTab").className =
+	"accoridon highlight";
+}
+function showCap() {
+	hidePanels();
+	document.getElementById("cap").style.display = "block";
+	document.getElementById("capTab").className =
+	"accoridon highlight";
+}
+function showF4() {
+	hidePanels();
+	document.getElementById("f4").style.display = "block";
+	document.getElementById("f4Tab").className =
+	"accoridon highlight";
+}
+function showGalactus() {
+	hidePanels();
+	document.getElementById("galactus").style.display = "block";
+	document.getElementById("galactusTab").className =
+	"accoridon highlight";
+}
+function showHulk() {
+	hidePanels();
+	document.getElementById("hulk").style.display = "block";
+	document.getElementById("hulkTab").className =
+	"accoridon highlight";
+}
+function showIron() {
+	hidePanels();
+	document.getElementById("iron").style.display = "block";
+	document.getElementById("ironTab").className =
+	"accoridon highlight";
+}
+function showPanther() {
+	hidePanels();
+	document.getElementById("panther").style.display = "block";
+	document.getElementById("pantherTab").className =
+	"accoridon highlight";
+}
+function showSurfer() {
+	hidePanels();
+	document.getElementById("surfer").style.display = "block";
+	document.getElementById("surferTab").className =
+	"accoridon highlight";
+}
+function showThor() {
+	hidePanels();
+	document.getElementById("thor").style.display = "block";
+	document.getElementById("thorTab").className =
+	"accoridon highlight";
+}
+function showVision() {
+	hidePanels();
+	document.getElementById("vision").style.display = "block";
+	document.getElementById("visionTab").className =
+	"accoridon highlight";
+}
+function showXmen() {
+	hidePanels();
+	document.getElementById("xmen").style.display = "block";
+	document.getElementById("xmenTab").className =
+	"accoridon highlight";
+}
+function hidePanels() {
+	document.getElementById("ant").style.display = "none";
+	document.getElementById("avengers").style.display = "none";
+	document.getElementById("cap").style.display = "none";
+	document.getElementById("f4").style.display = "none";
+	document.getElementById("galactus").style.display = "none";
+	document.getElementById("hulk").style.display = "none";
+	document.getElementById("iron").style.display = "none";
+	document.getElementById("panther").style.display = "none";
+	document.getElementById("surfer").style.display = "none";
+	document.getElementById("thor").style.display = "none";
+	document.getElementById("vision").style.display = "none";
+	document.getElementById("xmen").style.display = "none";
+	document.getElementById("antTab").className = "accordion";
+	document.getElementById("avengersTab").className = "accordion";
+	document.getElementById("capTab").className = "accordion";
+	document.getElementById("f4Tab").className = "accordion";
+	document.getElementById("galactusTab").className = "accordion";
+	document.getElementById("hulkTab").className = "accordion";
+	document.getElementById("ironTab").className = "accordion";
+	document.getElementById("pantherTab").className = "accordion";
+	document.getElementById("surferTab").className = "accordion";
+	document.getElementById("thorTab").className = "accordion";
+	document.getElementById("visionTab").className = "accordion";
+	document.getElementById("xmenTab").className = "accordion";
+}
